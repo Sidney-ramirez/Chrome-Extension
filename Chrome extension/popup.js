14dc758692a7673076f7b6835d3ac03006a466c9
Sidney-ramirez <sidney.ramirez@speeracademy.org>
2017-06-02 12:16:53 -0500

    v0.1.0.0 new and improved chrome extension

---
+++ Chrome extension/popup.js
@@ -0,0 +1,13 @@
+popup.js code: 
+document.addEventListener('DOMContentLoaded', function () {
+    var links = document.getElementsByTagName("a");
+    for (var i = 0; i < links.length; i++) {
+        (function () {
+            var ln = links[i];
+            var location = ln.href;
+            ln.onclick = function () {
+                chrome.tab.create({active: true, url: location});
+            };
+        })();
+    }
+});
